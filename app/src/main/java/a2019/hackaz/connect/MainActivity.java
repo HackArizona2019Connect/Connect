c9fa3650448c23206222ea6476584ff9e6ff6487
Luke De Mouy <lukedemouy@gmail.com>
2019-01-19 00:11:12 -0700

    add in the events activity and add in the intent to launch it from the drawer

--- app/src/main/java/a2019/hackaz/connect/MainActivity.java
+++ app/src/main/java/a2019/hackaz/connect/MainActivity.java
@@ -73,18 +73,12 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_information) {
             // Handle the camera action
+        } else if (id == R.id.nav_events) {
+            Intent switchActivity = new Intent(getApplicationContext(), eventsActivity.class);
+            startActivity(switchActivity);
         } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
